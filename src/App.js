ee5b7cedbd58877e340fb7ae594716cd4199341e
Douglas Perez <douglas.eduardo2000@gmail.com>
2021-09-17 12:26:17 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,87 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+
+import "swiper/swiper-bundle.css";
+
+import SwiperCore, {
+  Pagination,
+  Navigation,
+  Autoplay,
+  EffectFade,
+} from "swiper";
+
+import { getDogs } from "./dogapi";
+
+import "./App.css";
+
+// install Swiper modules
+SwiperCore.use([Pagination, Navigation, Autoplay, EffectFade]);
 
 function App() {
+  const [dogs, setDogs] = useState([]);
+
+  useEffect(() => {
+    const fetchDogs = async () => {
+      const doggies = await getDogs();
+
+      setDogs(doggies);
+    };
+
+    fetchDogs();
+  }, []);
+
+  const scaleImg = (e) => {
+    console.log(e.target);
+    e.target.classList.toggle("imgScale");
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Swiper
+        slidesPerView={1}
+        spaceBetween={30}
+        loop={true}
+        effect={"fade"}
+        pagination={{
+          clickable: true,
+        }}
+        navigation={true}
+        style={{
+          width: "80%",
+          height: "80vh",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        {dogs.map((dog, i) => (
+          <SwiperSlide
+            style={{
+              backgroundColor: "#fff",
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            <div style={{ position: "relative" }}>
+              <img
+                src={dog.url}
+                alt={dog.title}
+                style={{
+                  height: "400px",
+                  minWidth: "200px",
+                  width: "100%",
+                  maxWidth: "500px",
+                  transition: "all 300ms ease-in-out",
+                }}
+                onMouseOver={(e) => scaleImg(e)}
+                onMouseOut={(e) => scaleImg(e)}
+              />
+              <h2 style={{ fontFamily: "Open Sans" }}>{dog.title}</h2>
+            </div>
+          </SwiperSlide>
+        ))}
+      </Swiper>
     </div>
   );
 }
